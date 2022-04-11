3401cc1c01ece94b7920cf636cf3418d8fe4ccc7
lpalleros <lucaspalleros@gmail.com>
2022-04-11 18:24:44 -0300

    screen + components + fonts

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
+import {useState} from "react";
+import { useFonts } from "expo-font";
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {HOME, GAME} from "./constants/routes";
+import { TouchableOpacity } from "react-native";
+import { HomeScreen, GameScreen } from './screens';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [showScreen, setShowScreen] = useState(HOME);
+  const [loaded] = useFonts({
+    Poppins: require('./assets/fonts/Poppins/Poppins-LightItalic.ttf')
+  })
+
+  if(!loaded) return <AppLoading />
+  const handle = () => {
+    setShowScreen(GAME)
+  }
+  const goBack = () => {
+    setShowScreen(HOME)
+  }
+  
+  if(showScreen == HOME) { 
+    return (
+      <HomeScreen changeView={handle}/>
+    )
+  } else {
+    return (
+      <GameScreen changeView={goBack}/>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +38,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: 'Poppins',
   },
+  text: {
+    fontFamily: 'Poppins',
+  }
 });
